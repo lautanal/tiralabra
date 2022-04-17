4163bd7b7022e301f375f00b1e20954e5e9370a4
Lasse Lautanala <lasselautanala@gmail.com>
2022-04-17 16:59:00 +0300

    Yksikkötesti lisätty

--- src/tests/bestroute_test.py
+++ src/tests/bestroute_test.py
@@ -4,11 +4,18 @@ from node import Node
 from algorithm import Algorithm
 
 class TestBestroute(unittest.TestCase):
-    def initmap(self, maparray):
-        nrows = len(maparray)
-        ncols = len(maparray[0])
+    def initmap(self, costs):
+        if costs:
+            nrows = len(costs)
+            ncols = len(costs[0])
+        else:
+            nrows = 20
+            ncols = 20
         self.map = Map(nrows, ncols, 10)
-        self.map.set_costs(maparray)
+        if costs:
+            self.map.set_costs(costs)
+        else:
+            self.map.generate_costs()
         self.algorithm = Algorithm(self.map, None)
         node = self.map.nodes[0][0]
         node.set_start()
@@ -19,39 +26,39 @@ class TestBestroute(unittest.TestCase):
 
 
     def test_map1(self):
-        maparray = [['1','1','1','1','1','1'],
+        costs = [['1','1','1','1','1','1'],
                     ['1','1','1','1','1','1'],
                     ['1','1','1','1','1','1'],
                     ['1','1','1','1','1','1'],
                     ['1','1','1','1','1','1'],
                     ['1','1','1','1','1','1']]
-        self.initmap(maparray)
+        self.initmap(costs)
 
         result = self.algorithm.calculate()
         self.assertEqual(result[1], 9)
 
 
     def test_map2(self):
-        maparray = [['1','1','1','1','1','1'],
+        costs = [['1','1','1','1','1','1'],
                     ['1','1','1','1','1','1'],
                     ['1','1','1','1','1','1'],
                     ['1','1','1','1','1','1'],
                     ['B','B','B','B','B','B'],
                     ['1','1','1','1','1','1']]
-        self.initmap(maparray)
+        self.initmap(costs)
 
         result = self.algorithm.calculate()
         self.assertEqual(result[0], False)
 
 
     def test_map3(self):
-        maparray = [['1','1','2','2','2','2'],
+        costs = [['1','1','2','2','2','2'],
                     ['2','1','1','2','2','2'],
                     ['2','2','1','1','2','2'],
                     ['2','2','2','1','2','2'],
                     ['2','2','2','1','1','2'],
                     ['2','2','2','2','4','1']]
-        self.initmap(maparray)
+        self.initmap(costs)
 
         result1 = self.algorithm.calculate()
         self.algorithm.set_method()
@@ -61,16 +68,27 @@ class TestBestroute(unittest.TestCase):
 
 
     def test_map4(self):
-        maparray = [['1','1','1','1','1','1'],
+        costs = [['1','1','1','1','1','1'],
                     ['1','1','1','1','1','1'],
                     ['1','1','1','1','1','1'],
                     ['B','3','B','2','B','4'],
                     ['1','1','1','1','1','1'],
                     ['1','1','1','1','1','1']]
-        self.initmap(maparray)
+        self.initmap(costs)
 
         self.algorithm.set_method()
         self.algorithm.set_method()
         self.map.reset()
         result = self.algorithm.calculate()
         self.assertEqual(result[2], 10)
+
+
+    def test_map5(self):
+        self.initmap(None)
+
+        self.algorithm.set_diagonal()
+        result1 = self.algorithm.calculate()
+        self.algorithm.set_method()
+        self.map.reset()
+        result2 = self.algorithm.calculate()
+        self.assertEqual(result1[2], result2[2])
